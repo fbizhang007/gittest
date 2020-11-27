ca3278b85b29f6e520cd552afe9ae40a5766b025
zhangB <2236839293@qq.com>
2020-11-27 18:36:54 +0800

    hot-fix1

--- src/main/java/com/atguigu/gittest/Gittest.java
+++ src/main/java/com/atguigu/gittest/Gittest.java
@@ -8,6 +8,8 @@ import java.sql.SQLOutput;
  */
 public class Gittest {
     public static void main(String[] args) {
+        System.out.println();
+
         System.out.println("git1");
 
         System.out.println("git_hot-fix");
